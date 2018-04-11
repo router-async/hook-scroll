7fbae36046f94bc7905313880ce0373848fc1969
lamo2k123 <lamo2k123@gmail.com>
2018-04-11 10:49:01 +0300

    ooops fix

--- src/main.js
+++ src/main.js
@@ -1,50 +1,52 @@
 const storage = [];
 
-module.exports = ({ history, elementId, reset = true, limit = 20 }) => ({
+module.exports = ({ history, elementId, reset = true, limit = 20 }) => {
     const $el = elementId ? document.getElementById(elementId) : window;
     
-    start: () => {
-        if(history.action === 'PUSH') {
-            if($el) {
-                if(elementId) {
-                    storage.push([
-                        $el.scrollLeft,
-                        $el.scrollTop
-                    ]);
-                } else {
-                    storage.push([
-                        $el.scrollX,
-                        $el.scrollY
-                    ]);
+    return {
+        start: () => {
+            if(history.action === 'PUSH') {
+                if($el) {
+                    if(elementId) {
+                        storage.push([
+                            $el.scrollLeft,
+                            $el.scrollTop
+                        ]);
+                    } else {
+                        storage.push([
+                            $el.scrollX,
+                            $el.scrollY
+                        ]);
+                    }
+                }
+
+                if(storage.length > limit) {
+                    delete storage.shift();
                 }
             }
+        },
+        render: () => {
+            const { location } = history;
+            const state = location.state || {};
+            const { resetScroll,  resetOnAction = 'PUSH'} = state;
+            let position = [0, 0];
 
-            if(storage.length > limit) {
-                delete storage.shift();
-            }
-        }
-    },
-    render: () => {
-        const { location } = history;
-        const state = location.state || {};
-        const { resetScroll,  resetOnAction = 'PUSH'} = state;
-        let position = [0, 0];
+            switch (history.action) {
+                case 'POP':
+                    if(storage.length && !(resetScroll && resetOnAction === 'POP')) {
+                        position = storage.pop();
+                    }
 
-        switch (history.action) {
-            case 'POP':
-                if(storage.length && !(resetScroll && resetOnAction === 'POP')) {
-                    position = storage.pop();
-                }
-                
-                $el && $el.scrollTo(...position);
-                
-                break;
-            case 'PUSH':
-                if(reset || (resetScroll && resetOnAction === 'PUSH')) {
                     $el && $el.scrollTo(...position);
-                }
-                
-                break;
+
+                    break;
+                case 'PUSH':
+                    if(reset || (resetScroll && resetOnAction === 'PUSH')) {
+                        $el && $el.scrollTo(...position);
+                    }
+
+                    break;
+            }
         }
-    }
-});
+    };
+};
